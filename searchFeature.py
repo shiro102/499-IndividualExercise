9009065ac59c59b2389bf90ad375fb1b2caca8ed
Khai Hung <hungluongkhai@gmail.com>
2021-09-16 15:13:15 -0700

    Add unittest to check if feature work.

--- searchFeature.py
+++ searchFeature.py
@@ -1,2 +1,16 @@
+import unittest
+
+
 def search(string, key):
     return string.count(key)
+
+
+class TestSearchMethod(unittest.TestCase):
+
+    def test_search(self):
+        s = "abbccc"
+        self.assertEqual(search(s, 'b'), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
